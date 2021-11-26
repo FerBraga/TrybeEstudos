a25b2a790256dc68f70d9d65ca476891252528a4
Fernando Braga <augustofbg@gmail.com>
2021-11-26 19:35:45 -0300

    parte 4 feita

--- Fundamentos/4 - Javascript/4.1 - introdução Javascript/scripts.js
+++ Fundamentos/4 - Javascript/4.1 - introdução Javascript/scripts.js
@@ -1,11 +1,10 @@
-const num1= 47;
-const num2= 78;
-const num3= 95;
+const valor= 83;
 
-if (num1 > num2 && num1 > num3 ) {
-    console.log(num1);
-} else if (num1 < num2 && num2 < num3) {
-    console.log(num3);    
-} else{
-    console.log(num2);
+if (valor % 2 === 0 ) {
+    console.log("positive");
+
+} else if (valor % 2 !== 0) {
+    console.log("negative");
+} else { 
+    console.log("zero")
 }
