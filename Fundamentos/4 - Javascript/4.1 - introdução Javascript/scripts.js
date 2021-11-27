479c4225b370a779c590a2c3ceabce1b86965782
Fernando Braga <augustofbg@gmail.com>
2021-11-27 14:16:02 -0300

    parte 6 feita

--- Fundamentos/4 - Javascript/4.1 - introdução Javascript/scripts.js
+++ Fundamentos/4 - Javascript/4.1 - introdução Javascript/scripts.js
@@ -1,9 +1,18 @@
-const ang1= 32;
-const ang2= 45;
-const ang3= 65;
+let peca1= "bispo";
 
-if (ang1 + ang2 + ang3 === 180 ) {
-    console.log(true);
+
+if (peca1=== "bispo"){
+    console.log("movimentos diagonais");
+} else if ( peca1==="cavalo"){
+    console.log("movimentos em 'L'");
+} else if (peca1==="rainha"){
+    console.log("movimentos para todos os lados");
+} else if (peca1==="torre"){
+    console.log("movimentos para frente, para traz e para os lados");
+} else if (peca1==="rei"){
+    console.log("movimentos para todos os lados");
+} else if (peca1==="peão"){
+    console.log("movimentos para frente e para os lados");
 } else {
-    console.log(false); 
-}    
+    console.log("Peça inválida.")
+}
