360ee81dec819c2d55a344d084652d758857eb4a
Fernando Braga <augustofbg@gmail.com>
2021-11-29 11:43:16 -0300

    parte 8 feita

--- Fundamentos/4 - Javascript/4.1 - introdução Javascript/scripts.js
+++ Fundamentos/4 - Javascript/4.1 - introdução Javascript/scripts.js
@@ -1,17 +1,9 @@
-let nota= [63];
+const num1= [7];
+const num2= [5];
+const num3= [9];
 
-if (nota>= 90){
-console.log("A");
-} else if (nota>=80){
-    console.log("B");
-} else if (nota>= 70){
-    console.log("C");
-} else if (nota>=60){
-    console.log("D");
-} else if (nota>=50){
-    console.log("E");
-} else if (nota<50){
-    console.log("F");
-} else if (nota <0 || nota> 100 ){
-    console.log("Erro!")
-}
+if (num1%2===0 || num2%2===0 || num3%2===0 ){
+    console.log(true);
+} else {
+    console.log(false);
+}   
