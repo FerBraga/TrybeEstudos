2b298aa575ba47d98acc2ebb416994203694b0ea
Fernando Braga <augustofbg@gmail.com>
2021-11-29 11:48:30 -0300

    parte 9 feita

--- Fundamentos/4 - Javascript/4.1 - introdução Javascript/scripts.js
+++ Fundamentos/4 - Javascript/4.1 - introdução Javascript/scripts.js
@@ -1,8 +1,8 @@
-const num1= [7];
-const num2= [5];
-const num3= [9];
+const num1= [8];
+const num2= [10];
+const num3= [7];
 
-if (num1%2===0 || num2%2===0 || num3%2===0 ){
+if (num1%2!==0 || num2%2!==0 || num3%2!==0 ){
     console.log(true);
 } else {
     console.log(false);
