326844ad56b68401a338b17fda587cd87ad54a4c
Fernando Braga <augustofbg@gmail.com>
2021-12-01 19:59:14 -0300

    segundo exercicio do marta

--- Fundamentos/4 - Javascript/4.4 - Objetos e For in/treino.js
+++ Fundamentos/4 - Javascript/4.4 - Objetos e For in/treino.js
@@ -6,5 +6,6 @@ let player = {
      golden: 2, 
      silver: 3,
     },
+    bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018],
 }
-console.log("A jogadora" + " " + player.name + " " + player.lastName + " " +"tem" + " " + player.age)
+console.log("A jogadora Marta foi eleita a melhor jogadora do mundo, 6 vezes:" + " " + player.bestInTheWorld)
