fba2672284639c7a5ae2ee960c5ad9157a84a895
Fernando Braga <augustofbg@gmail.com>
2021-12-01 15:52:34 -0300

    parte tres inacabada

--- Fundamentos/4 - Javascript/4.3 - Lógica e algoritmo/scripts.js
+++ Fundamentos/4 - Javascript/4.3 - Lógica e algoritmo/scripts.js
@@ -1,8 +1,12 @@
 let n = 5;
 let simbolo = '*';
 let line = '';
+let 
 
-for (let index = 0; index < n; index += 1){
+for (let index = 0; index < n; index +=1){
   line= line + simbolo;
   console.log(line);
+for ( let index = 0; index =1; index+=1){
+    console.log(espaco)
+  }
 }
