bc4ec8ae6acd04b8c86108811bfe50e300907d8d
Fernando Braga <augustofbg@gmail.com>
2021-12-02 11:50:00 -0300

    primeiro para fixar for in

--- Fundamentos/4 - Javascript/4.4 - Objetos e For in/treino.js
+++ Fundamentos/4 - Javascript/4.4 - Objetos e For in/treino.js
@@ -1,11 +1,8 @@
-let player = {
-    name: 'Marta',
-    lastName: 'Silva',
-    age: 34,
-    medals: {
-     golden: 2, 
-     silver: 3,
-    },
-    bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018],
-}
-console.log(" A jogadora possui" + " " + medals.golden + " " + "medalhas de ouro, e " + " " + medals.silver + " " + "medalhas de prata.")
+let names = {
+    person1: "João",
+    person2: 'Maria',
+    person3: 'Jorge',
+  };
+  for (let key in names){
+    console.log("Olá," + names[key] + "!")
+  }
