b7f03a4ae081ec3a98ccedc3bfce45ae424e107c
Fernando Braga <augustofbg@gmail.com>
2021-12-03 18:10:03 -0300

    ultimos do bloco 4

--- Fundamentos/4 - Javascript/4.4 - Objetos e For in/treino.js
+++ Fundamentos/4 - Javascript/4.4 - Objetos e For in/treino.js
@@ -1,13 +1,10 @@
-let leitor = {
-    nome: 'Julia',
-    sobrenome: 'Pessoa',
-    idade: 21,
-    livrosFavoritos: [
-      {
-        titulo: 'O Pior Dia de Todos',
-        autor: 'Daniela Kopsch',
-        editora: 'Tordesilhas',
-      },
-    ],
-  };
-  console.log("O livro favorito de " + leitor.nome + " " + leitor.sobrenome + " " + "se chama" + " " + leitor.livrosFavoritos[0].titulo ) 
+let palavra = "arara";
+
+function palindromo(palavra){
+    if (palavra.reverse() === palavra){
+        return true;
+    } else {
+        return  false;
+    }
+}
+console.log(palindromo("arara"))
