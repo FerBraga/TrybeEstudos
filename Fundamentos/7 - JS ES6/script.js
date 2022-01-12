0b1adccccc887c762e910dd66824ef17894c03e4
Fernando Braga <augustofbg@gmail.com>
2022-01-12 19:45:25 -0300

    feito primeira parte dos exercicios do dia

---
+++ Fundamentos/7 - JS ES6/script.js
@@ -0,0 +1,20 @@
+// const test = (escopo) => {
+//     if (escopo === true) {
+//       var ifScope = 'Não devo ser utilizada fora do meu escopo (if),';
+//       console.log(`${ifScope}  ótimo, fui utilizada no escopo !'`);
+//     } else {
+//       var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+//       console.log(elseScope);
+//     }
+//   }
+
+//   test(true);
+
+  const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+  const alteraOrdem = oddsAndEvens.sort(function(a, b){return a-b});
+
+  console.log(`Os números ${alteraOrdem} , se encontram ordenados de forma crescente`); 
+
+
+  
