fc2dbbd08dfb63e60febd46a3e9529793ac275f9
Fernando Braga <augustofbg@gmail.com>
2022-01-13 10:44:23 -0300

    feito parte 1

--- Fundamentos/7 - JS ES6/script.js
+++ Fundamentos/7 - JS ES6/script.js
@@ -1,14 +1,14 @@
-// const test = (escopo) => {
-//     if (escopo === true) {
-//       var ifScope = 'Não devo ser utilizada fora do meu escopo (if),';
-//       console.log(`${ifScope}  ótimo, fui utilizada no escopo !'`);
-//     } else {
-//       var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-//       console.log(elseScope);
-//     }
-//   }
+const test = (escopo) => {
+    if (escopo === true) {
+      var ifScope = 'Não devo ser utilizada fora do meu escopo (if),';
+      console.log(`${ifScope}  ótimo, fui utilizada no escopo !'`);
+    } else {
+      var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+      console.log(elseScope);
+    }
+  }
 
-//   test(true);
+  test(true);
 
   const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
